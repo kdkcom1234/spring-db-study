a4328d604e5ff55e3e9de88982b27e9031856505
Daekeun Ko <kdkcom1234@gmail.com>
2023-07-27 13:06:40 +0900

    POST 구현

--- controller/src/main/java/com/tje/controller/post/Post.java
+++ controller/src/main/java/com/tje/controller/post/Post.java
@@ -1,10 +1,15 @@
 package com.tje.controller.post;
 
-import lombok.Builder;
-import lombok.Data;
+import lombok.*;
+import org.springframework.http.HttpStatus;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Data
 @Builder
+@AllArgsConstructor // 전체 필드 초기 생성자
+@NoArgsConstructor  // 빈 생성자
 public class Post {
     private long no;
     private String title;
@@ -12,3 +17,4 @@ public class Post {
     private String creatorName;
     private long createdTime;
 }
+
--- controller/src/main/java/com/tje/controller/post/PostController.java
+++ controller/src/main/java/com/tje/controller/post/PostController.java
@@ -2,11 +2,9 @@ package com.tje.controller.post;
 
 
 import com.tje.controller.contact.Contact;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
@@ -14,6 +12,7 @@ import java.util.concurrent.ConcurrentLinkedDeque;
 import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicLong;
 
 // GET /posts
 // 게시글 목록이 JSON으로 나오게
@@ -23,8 +22,8 @@ public class PostController {
     // 동시성을 위한 자료 구조
     // HashMap -> ConcurrentHashMap
     // Integer -> AtomicInteger
-    Map<Integer, Post> map = new ConcurrentHashMap<>();
-    AtomicInteger num = new AtomicInteger(0);
+    Map<Long, Post> map = new ConcurrentHashMap<>();
+    AtomicLong num = new AtomicLong(0);
 
     // post 목록 화면을 제작 post.html, post.js
     // fetch api를 사용하여 /posts 주소를 호출한 후
@@ -84,19 +83,38 @@ public class PostController {
 
     // title, content 필수 속성
     // creatorName: 서버에서 가짜데이터로 넣음
-//    @PostMapping
-//    public ResponseEntity<Map<String, Object>> addPost(...) {
-    // 1. 입력값 검증(title, content)
-    //  -> 입력값 오류(빈 값)가 있으면 400 에러 띄움
+    @PostMapping
+    public ResponseEntity<Map<String, Object>> addPost(@RequestBody Post post) {
+//     1. 입력값 검증(title, content)
 
-    // 2. 채번: 번호를 딴다(1 .. 2, 3....)
-    //    int no = num.incrementAndGet();
+//      -> 입력값 오류(빈 값)가 있으면 400 에러 띄움
+        if(post.getTitle() == null || post.getContent() == null || post.getContent().isEmpty() || post.getTitle().isEmpty()){
+            Map<String, Object> response = new HashMap<>();
+            response.put("data", null);
+            response.put("message", "[title] and [content] is Required Field");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+        }
 
-    // 3. 번호(no), 시간값(createdTime) 게시자이름(creatorName) 요청 객체에 설정
-    // (set필드명(...))
+//     2. 채번: 번호를 딴다(1 .. 2, 3....)
+        long no = num.incrementAndGet();
+        post.setNo(no);
 
-    // 4. 맵에 추가
+//     3. 번호(no), 시간값(createdTime) 게시자이름(creatorName) 요청 객체에 설정
+        post.setCreatorName("Dodo");
+        post.setCreatedTime(new Date().getTime());
 
-    // 5. 생성된 객체를 맵에서 찾아서 반환, 201
-//    }
+//     4. 맵에 추가
+        map.put(no, post);
+        System.out.println(post);
+
+//     5. 생성된 객체를 맵에서 찾아서 반환, 201
+        Map<String, Object> res = new HashMap<>();
+        System.out.println(post.getNo());
+        res.put("data", map.get(no));
+        res.put("message", "created");
+
+        System.out.println(ResponseEntity.status(HttpStatus.CREATED).body(res));
+//        return ResponseEntity.status(HttpStatus.CREATED).build();
+        return ResponseEntity.status(HttpStatus.CREATED).body(res);
+    }
 }
