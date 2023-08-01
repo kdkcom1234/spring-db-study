dcc9ec8516c06ff908dcd2442a0f165f60f4f587
Daekeun Ko <kdkcom1234@gmail.com>
2023-08-01 12:50:20 +0900

    query creation 컨트롤러 추가

--- myapp/src/main/java/com/kdk/myapp/contact/ContactController.java
+++ myapp/src/main/java/com/kdk/myapp/contact/ContactController.java
@@ -1,10 +1,7 @@
 package com.kdk.myapp.contact;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Example;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.*;
 import org.springframework.data.repository.query.FluentQuery;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
