6ba11bfbabf28464131c9efc0f52eaa2f3a340f1
Daekeun Ko <kdkcom1234@gmail.com>
2023-08-01 09:53:12 +0900

    native query 설명 추가

--- myapp/src/main/java/com/kdk/myapp/contact/ContactRepository.java
+++ myapp/src/main/java/com/kdk/myapp/contact/ContactRepository.java
@@ -11,11 +11,18 @@ import java.util.Optional;
 // JpaRepository<Contact, String>
 // <엔티티클래스, 엔티티의키타입>
 public interface ContactRepository extends JpaRepository<Contact, String> {
+    // JPA Native query 사용
+    // ANSI SQL이 사용가능할 때만 사용하는게 좋다
+    // INSERT, UPDATE, DELETE는 JPA의 기본 메서드를 사용한다.
+    // repository.save(entity), repository.deleteById(id);
+
+    //@Query(value="SQL 문법", nativeQuery = true)
     @Query(value = "select * " +
             "from contact " +
             "order by name asc ", nativeQuery = true)
     List<Contact> findContactsSortByName();
 
+    //@Query(value="SQL 문법 :매개변수", nativeQuery = true)
     @Query(value = "select * " +
             "from contact " +
             "where email = :email ", nativeQuery = true)
