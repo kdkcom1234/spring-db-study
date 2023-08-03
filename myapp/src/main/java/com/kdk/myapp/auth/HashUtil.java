b179c37b4eca89e578b9dcc0df44fa690794d878
Daekeun Ko <kdkcom1234@gmail.com>
2023-08-03 18:14:57 +0900

    주석 추가

--- myapp/src/main/java/com/kdk/myapp/auth/HashUtil.java
+++ myapp/src/main/java/com/kdk/myapp/auth/HashUtil.java
@@ -4,6 +4,9 @@ import at.favre.lib.crypto.bcrypt.BCrypt;
 
 public class HashUtil {
     public String createHash(String cipherText) {
+        // https://www.baeldung.com/java-password-hashing
+        // https://mia-dahae.tistory.com/120
+
         // https://blog.kakaocdn.net/dn/IdglW/btrEoJ6HDUJ/FmJqCChB9NCXd6fapmJdAk/img.png
         // 'hashToString': Salt와 함께 해시를 생성
         return BCrypt
